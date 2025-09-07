6c0151bde1ee9ed0a47eb5d791d01dafe4f3c5d3
ramsgitrepo3 <ramstechworld03@gmail.com>
2025-09-08 00:39:37 +0530

    third commit

--- src/poda/Rams.java
+++ src/poda/Rams.java
@@ -1,7 +1,18 @@
 package poda;
 
+
+
 public class Rams {
 
 	
+	int a = 5;
+	int b = a++ + ++a + a-- - --a;
+	
+	public static void main(String[] arg) {
+		
+		Rams cl=new Rams();
+		System.out.println(cl.b);
+	}
 	
 }
+
