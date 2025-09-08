f19d5bd1cb98f2b91a0a3ed0c1ce922ad5f877ba
kumarmaran <ramkumarselvam03@gmail.com>
2025-09-08 08:26:10 +0530

    kumar commit

---
+++ src/poda/Threesum.java
@@ -0,0 +1,17 @@
+package poda;
+
+public class Threesum {
+
+	
+	public static void main(String[] arg) {
+		
+		int number=10;
+		
+		for(int i=2;i<=number;i++) {
+			System.out.println("hello"+i)	;	
+			
+			System.setOut(null);
+			
+	}
+}
+}
